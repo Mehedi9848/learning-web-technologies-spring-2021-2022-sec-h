6cf6df26643e4b7aa871a9865b2c97d488d05b1b
Mehedi9848 <mhassandulal@gmail.com>
2022-02-21 18:17:31 -0500

    MID_LAB_TASK_04_PHP

---
+++ MID_LAB_TASK_04_PHP/Task6.php
@@ -0,0 +1,10 @@
+<?php
+	$students = [
+					's1'=> ['id'=>1, 'name'=>'Md.', 'cgpa'=>4.00, 'email'=>'abc.edu'],
+					's2'=> ['id'=>2, 'name'=>'Mehedi', 'cgpa'=>3.16, 'email'=>'def.edu'],
+					's3'=> ['id'=>3, 'name'=>'Hassan', 'cgpa'=>1.00, 'email'=>'ghi.edu']
+				];
+		echo $students['s1']['cgpa'];		
+
+
+?>
