4753d02a0759976f37989e84c7c8ea4c476e67d2
Md. Mehedi Hassan <99037463+Mehedi9848@users.noreply.github.com>
2022-03-14 23:05:47 -0800

    Update Profile.php

--- Project/loginPage/Profile.php
+++ Project/loginPage/Profile.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+$username = "";
+if(isset($_SESSION['current_user']))
+{
+	$username= $_SESSION['current_user']['username'];
+}
+?>
 <html>
 <head>
 	<title>Profile</title>
@@ -9,40 +17,26 @@
 
 	<form method="POST" action="">
 		<fieldset>
-			<legend>Dashboard</legend>
+			<legend>Profile</legend>
 			<table>
 				<tr>
-					<td>Course registration</td>
-					<td><input type="text" name="Course registration" value=""></td>
-				</tr>
-				<tr>
-					<td>Make Payment</td>
-					<td><input type="Make Payment" name="Make Payment" value=""></td>
-				</tr>
-				<tr>
-					<td>Routine</td>
-					<td><input type="Routine" name="Routine" value=""></td>
-				</tr>
-				<tr>
-					<td>Course Materials</td>
-					<td><input type="Course Make" name="Course Materials" value=""></td>
-				</tr>
-				
-				<tr>
-					<td>Courses and Grades</td>
-					<td><input type="Courses and Grades" name="Courses and Grades" value=""></td>
-				</tr>
-				<tr>
-					<td> Applications</td>
-					<td><input type="Applications" name="Applications" value=""></td>
-				</tr>
-				<tr>
-					<td>Review</td>
-					<td><input type="Review" name="Review" value=""></td>
-				</tr>
-				<tr>
-					<td>Edit Profile</td>
-					<td><input type="Edit Profile" name="Edit Profile" value=""></td>
+					<ul>
+					<li><a href="CourseRegistration.php"> Course Registration</a></li>
+					
+					<li><a href="Make Payment.php"> Make Payment</a></li>
+
+					<li><a href="Routine.php"> Routine</a></li>
+					
+					<li><a href="Course Materials.php"> Course Materials</a></li>
+					
+					<li><a href="Courses and Grades.php"> Courses and Grades</a> </li>
+					
+					<li><a href="Applications.php"> Applications </a></li>
+					
+					<li><a href="Review.php"> Review</a></li>
+					
+					<li><a href="Edit Profile.php"> Edit Profile</a></li>
+					</ul>
 				</tr>
 
 				
